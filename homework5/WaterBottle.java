f2e916fa9efcada28a2ef5e43fdcc092998c2675
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2019-06-04 02:24:06 +0300

    Add files via upload

---
+++ homework5/WaterBottle.java
@@ -0,0 +1,13 @@
+package homework5;
+
+public class WaterBottle {
+
+    private String brand;
+    private boolean empty;
+
+    public static void main(String[] args) {
+        WaterBottle wb = new WaterBottle();
+        System.out.print("Empty = " + wb.empty);
+        System.out.print(", Brand = " + wb.brand);
+    }
+}
