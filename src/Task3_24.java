d1bb1b3c13df29be84716d40293caa240fd31709
slavic-sky <zirchik1997@mail.ru>
2019-06-04 16:41:06 +0300

    Revert "restructured packaged 1"
    This reverts commit 23d2354b9ec9bdc7c3719555dacf69d776a2f345.

---
+++ src/Task3_24.java
@@ -0,0 +1,17 @@
+public class Task3_24 {
+
+    private static boolean[] firstArgument = {true, false};
+    private static boolean[] secondArgument = {true, false};
+    private static boolean[] thirdArgument = {true, false};
+
+    public static void main(String[] args) {
+        for (boolean i: firstArgument){
+            for (boolean j: secondArgument)
+                for (boolean k: thirdArgument){
+                System.out.println("X = " + i + " Y = " + j + " Z = " + k + " не (Y или не X и Z) или Z = " + (!(j || !i & k) || k));
+                System.out.println("X = " + i + " Y = " + j + " Z = " + k + " X и не (не Y или Z) или Y = " + (i & !(!j || k) || j));
+                System.out.println("X = " + i + " Y = " + j + " Z = " + k + " не (X или Y и Z) или не X = " + (!(i || j & k) || !i));
+            }
+        }
+    }
+}
